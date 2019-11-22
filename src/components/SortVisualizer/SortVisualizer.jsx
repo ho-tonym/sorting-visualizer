491e0078b4761c9aaa20a71cf8736e836b948125
ho-tonym <ho.tonym@gmail.com>
2019-11-22 16:00:43 -0500

    add pause/resume functionality

--- src/components/SortVisualizer/SortVisualizer.jsx
+++ src/components/SortVisualizer/SortVisualizer.jsx
@@ -5,7 +5,7 @@ import { mergeSort, mergeTestSortingAlgorithms } from './algorithms/mergeSort/me
 // import { bubbleSort } from './algorithms/bubbleSort/bubbleSort'
 import { Navbar, Button } from 'react-bootstrap'
 
-const ARRAY_BARS_COUNT = 10
+const ARRAY_BARS_COUNT = 100
 const BAR_COLOR = "#2ad19d"
 // const BAR_COLOR = "#ff9f38"
 const ANIMTION_TIME = 10
@@ -62,7 +62,8 @@ class SortVisualizer extends React.Component {
     this.state = {
       array: [],
       permutationsToExecute: [],
-      isSorted: false
+      isSorted: false,
+      isRunning: false
     }
   }
 
@@ -81,20 +82,20 @@ class SortVisualizer extends React.Component {
     for (let i = 0; i < ARRAY_BARS_COUNT; i++) {
       array.push(randomInt(5, 400))
     }
-      this.setState({array, permutationsToExecute: array, isSorted: false})
+      this.setState({
+        array,
+        permutationsToExecute: array,
+        isSorted: false,
+        isRunning: false
+      })
   }
 
   swapAnimation = (j, k) => {
     const tempArray = this.state.array
     const tempElement = tempArray[j]
-    console.log(this.state.permutationsToExecute)
-    console.log(j, k)
-    console.log("state  " + this.state.array)
+
     tempArray[j] = tempArray[k];
     tempArray[k] = tempElement;
-
-    console.log("tstate " + tempArray)
-    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 
     this.setState((prevState) => ({
       array: tempArray,
@@ -114,7 +115,8 @@ class SortVisualizer extends React.Component {
     const {permutationsToExecute} = this.state
     permutationsToExecute.length > 0 ? this.swapAnimation(permutationsToExecute[0][0], permutationsToExecute[0][1]) :
     this.setState({
-      isSorted: true
+      isSorted: true,
+      isRunning: true
     })
 
   }
@@ -137,6 +139,13 @@ class SortVisualizer extends React.Component {
         this.interval = setInterval(this.executeAnimation, ANIMTION_TIME)
       }
     )//done
+  }
+
+  pauseResume = () => {
+    this.state.isRunning ? this.interval = setInterval(this.executeAnimation, ANIMTION_TIME) : clearInterval(this.interval);
+    this.setState((prevState) => ({
+      isRunning: !prevState.isRunning
+    }))
   }
 
   render() {
@@ -166,6 +175,7 @@ class SortVisualizer extends React.Component {
             this.bubbleSort(array)
           }}>Bubble Sort</Button>
           <Button variant="primary" onClick={() => mergeTestSortingAlgorithms()}>Test Function</Button>
+          <Button variant="primary" onClick={this.pauseResume}>PAUSE</Button>
         </Navbar>
 
       </>
