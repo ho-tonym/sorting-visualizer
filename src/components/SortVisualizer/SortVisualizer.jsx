21e74a7a0682690c8e7972f60d552e75dc491135
ho-tonym <ho.tonym@gmail.com>
2019-11-22 17:25:42 -0500

    add color to indicate which bars are being compared

--- src/components/SortVisualizer/SortVisualizer.jsx
+++ src/components/SortVisualizer/SortVisualizer.jsx
@@ -5,54 +5,12 @@ import { mergeSort, mergeTestSortingAlgorithms } from './algorithms/mergeSort/me
 // import { bubbleSort } from './algorithms/bubbleSort/bubbleSort'
 import { Navbar, Button } from 'react-bootstrap'
 
-const ARRAY_BARS_COUNT = 100
+const ARRAY_BARS_COUNT = 20
 const BAR_COLOR = "#2ad19d"
-// const BAR_COLOR = "#ff9f38"
+const BAR_DONE_COLOR = "#ff9f38"
+const BAR_COMPARE_COLOR = "#4d84fe"
+
 const ANIMTION_TIME = 10
-// ADD IN FINISHED COLOR
-
-
-
-
-
-
-// function swapAnimation(array, j, k) {
-//   const arrayBars = document.getElementsByClassName('sort-visualizer');
-//
-//   let barOneStyle = arrayBars[0].childNodes[j].style
-//   let barTwoStyle = arrayBars[0].childNodes[k].style
-//
-//   barOneStyle["background-color"] = "#4d84fe";
-//   barTwoStyle["background-color"] = "#4d84fe";
-//   setTimeout(function () {
-//     barOneStyle["background-color"] = "#2ad19d";
-//     barTwoStyle["background-color"] = "#2ad19d";
-//
-//     let tempHeight = barOneStyle["height"]
-//     barOneStyle["height"] = barTwoStyle["height"]
-//     barTwoStyle["height"] = tempHeight
-//
-//
-//     if (k >= lastNumber){
-//       //NOT BEING CALLED ENOUGH
-//       // console.log("j: "+ j, "k: " + k, "lastNumber: "+ lastNumber, "numberToAdjust: "+ numberToAdjust)
-//       lastNumber = k
-//     }
-//     else{
-//       // console.log("j: "+ j, "k: " + k, "lastNumber: "+ lastNumber, "numberToAdjust: "+ numberToAdjust + "c")
-//       arrayBars[0].childNodes[numberToAdjust].style["background-color"] = "#ff9f38";
-//       numberToAdjust--
-//       lastNumber = 0
-//     }
-//     currentIteration++
-//     // console.log(maxIterations, currentIteration)
-//     if(currentIteration === maxIterations){
-//       for (var i = 0; i < arrayBars[0].childNodes.length; i++) {
-//         arrayBars[0].childNodes[i].style["background-color"] = "#ff9f38";
-//       }
-//     }
-//   }, ANIMTION_TIME)
-// }
 
 
 class SortVisualizer extends React.Component {
@@ -63,7 +21,8 @@ class SortVisualizer extends React.Component {
       array: [],
       permutationsToExecute: [],
       isSorted: false,
-      isRunning: false
+      isRunning: false,
+      comparedValues: []
     }
   }
 
@@ -99,7 +58,8 @@ class SortVisualizer extends React.Component {
 
     this.setState((prevState) => ({
       array: tempArray,
-      permutationsToExecute: prevState.permutationsToExecute.slice(1)
+      permutationsToExecute: prevState.permutationsToExecute.slice(1),
+      comparedValues: [j,k]
     }))
   }
 
@@ -116,9 +76,8 @@ class SortVisualizer extends React.Component {
     permutationsToExecute.length > 0 ? this.swapAnimation(permutationsToExecute[0][0], permutationsToExecute[0][1]) :
     this.setState({
       isSorted: true,
-      isRunning: true
+      isRunning: true,
     })
-
   }
 // if we using this incomponent we need arrow functions
   bubbleSort = (arr) => {
@@ -149,7 +108,7 @@ class SortVisualizer extends React.Component {
   }
 
   render() {
-    const { array } = this.state
+    const { array, comparedValues, isRunning } = this.state
     return (
       <>
         <div className="sort-visualizer">
@@ -158,7 +117,9 @@ class SortVisualizer extends React.Component {
               <div
                 key={id}
                 style={{
-                  backgroundColor: BAR_COLOR,
+                  backgroundColor:
+                    `${isRunning ? BAR_COLOR : id === comparedValues[0] || id === comparedValues[1] ? BAR_COMPARE_COLOR : BAR_COLOR}`
+                  ,
                   height: `${value}px`,
                 }}>
               </div>
