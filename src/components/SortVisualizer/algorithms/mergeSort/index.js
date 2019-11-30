96504d030118b3b7a65730ef319b6315361013b6
ho-tonym <ho.tonym@gmail.com>
2019-11-30 13:59:56 -0500

    start jest

--- src/components/SortVisualizer/algorithms/mergeSort/index.js
+++ src/components/SortVisualizer/algorithms/mergeSort/index.js
@@ -2,33 +2,36 @@ let tempArray = []
 let animationArray = []
 
 export function startMergeSort(arr) {
-  if (arr.length <= 1) return arr;
+  const length = arr.length
+  if (length <= 1) return arr;
   let tempArray = [...arr]
   let array = [...arr]
   animationArray = []
-  mergeSortHelper(array, 0, array.length - 1)
+
+  mergeSortHelper(array, 0, length - 1)
   return animationArray
 }
 
 function mergeSortHelper(array, leftStart, rightEnd) {
   if (leftStart === rightEnd) return
   const middle = Math.floor((leftStart + rightEnd) / 2);
+
   mergeSortHelper(array, leftStart, middle)
   mergeSortHelper(array, middle + 1, rightEnd)
   doMerge(array, leftStart, rightEnd)
 }
 
 function doMerge(array, leftStart, rightEnd) {
-  let leftEnd = Math.floor((rightEnd + leftStart) / 2);
-  let rightStart = leftEnd + 1
-  let size = rightEnd - leftStart + 1
+  const leftEnd = Math.floor((rightEnd + leftStart) / 2);
+  const rightStart = leftEnd + 1
+  const size = rightEnd - leftStart + 1
 
   let left = leftStart
   let right = rightStart
   let index = leftStart
 
   while(left <= leftEnd && right <= rightEnd) {
-    if (array[left] <= array[right]){
+    if (array[left] <= array[right]) {
       tempArray[index] = array[left]
       left++
     } else {
@@ -38,28 +41,16 @@ function doMerge(array, leftStart, rightEnd) {
     index++
   }
 
-
-  arraycopy(array, left, tempArray, index, leftEnd - left + 1)
-  arraycopy(array, right, tempArray, index, rightEnd - right + 1)
-  arraycopy(tempArray, leftStart, array, leftStart, size)
+  arrayCopy(array, left, tempArray, index, leftEnd - left + 1)
+  arrayCopy(array, right, tempArray, index, rightEnd - right + 1)
+  arrayCopy(tempArray, leftStart, array, leftStart, size)
 }
-// 0, 1, 1
-// 2, 1, 0
-function arraycopy(fromArr, fromPos, toArr, toPos, length) {
 
-  for (var i = 0; i < length; i++) {
-    // console.log(fromPos, fromArr[fromPos])
+function arrayCopy(fromArr, fromPos, toArr, toPos, length) {
+  for (let i = 0; i < length; i++) {
     toArr[toPos] = fromArr[fromPos]
     animationArray.push([fromPos, toArr[toPos]])
-
     toPos++
     fromPos++
   }
-  // console.log("end", fromArr, fromPos, toArr, toPos, length)
 }
-//
-// const x = [1,2,3,4]
-// const y = [5,6,7,8]
-// arraycopy(x, 0, y, 1, 1)
-// console.log(x, y)
-// startMergeSort([1,26,3,4])
